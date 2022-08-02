0e600222a76d496bdafd81497a4aadc99800ef05
Pavithran <pavithrank02@gmail.com>
2022-08-02 08:48:30 +0530

    later added

--- pract.js
+++ pract.js
@@ -6,9 +6,33 @@
 // const firstName = 'Pavi';
 // calcAge(1997); 
 'use strict'
-console.log(this);
-const calcAge = function (birthYear) {
-   console.log(2022 - birthYear)
-    console.log(this);
-};
-calcAge(1997);
+// console.log(this);
+// const calcAge = function (birthYear) {
+//    console.log(2022 - birthYear)
+//     console.log(this);
+// };
+// calcAge(1997);
+// let age = 30; 
+// let oldAge = age;
+// age = 31;
+// console.log(age);
+// console.log(oldage);
+
+// const me = {
+//     name: 'pavi',
+//     age: 25,
+// };
+// const friend = me;
+// friend.age = 27;
+
+let lastname = 'Williams';
+let oldLastName = lastname;
+lastName = 'Davis';
+
+const jessica = {
+    firstName: 'jessica',
+    lastName: 'williams',
+    age: 27,
+}
+const marriedJessica = jessica;
+
