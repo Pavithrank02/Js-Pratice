2f55277c9c1886725c95e9667ee3796af1413f7b
Pavithran <pavithrank02@gmail.com>
2022-08-03 21:51:26 +0530

    objects and maps

---
+++ array.js
@@ -0,0 +1,45 @@
+
+const restaurant = {
+    name: 'classico Italiano',
+    location: 'via agenl, frience, italy',
+    categories: ['italian', 'Veg', 'Pizza', 'Org'],
+    startMenu: ['garlic', 'foccacia', 'salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    order: function(startIndex, mainIndex) {
+        
+    }
+};
+// const array = [1,2,3];
+
+// const [a, b, c] = array; 
+// let [main, secondary] = array;
+// console.log(main, secondary);
+
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
+const arr = [1, 2, ...[3, 4]];
+
+const [a, b, ...others] = [1,2,3,4,5];
+
+const [pizza, Risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.startMenu];
+console.log(pizza, Risotto, otherFood);
+
+const add = function() {
+    
+}
+
+const menu = [...restaurant.startMenu, ...restaurant.mainMenu];
+for ( const items of menu) 
+console.log(items)
+
+const recipies = object.keys(order);
+console.log(recipies)
+;
+
+const rest = new Map();
+rest.set('name', 'classico italiano');
+rest.set(1, 'firence, italy');
+rest.set(2, 'lisbon, portugal');
+rest.set('categories', ['italian', 'Veg', 'Pizza', 'Org']).set('open', 11).set('close', 23).set(true, 'we open at')
+console.log(rest.get('name'));
